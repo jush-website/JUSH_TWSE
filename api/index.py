b7a2850156bd5652db3e6060473bfb7a8fd9d37e
Jush <102575403+jush-website@users.noreply.github.com>
2026-05-20 11:24:42 +0800

    Delete api directory

--- api/index.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-# Add the project root to sys.path so we can import from 'src'
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from src.web_app import app
-
-# Vercel treats this 'app' as the ASGI entry point
