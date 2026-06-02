9d42446419683da102032e33d4f30d86dd3ff91a
Jush <jim635241@gmail.com>
2026-06-02 22:18:00 +0800

    2

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
 from src.backend.data_fetcher import DataFetcher
+from datetime import datetime, timedelta
 f = DataFetcher()
-print(f.fm_loader.taiwan_stock_per_pbr(stock_id='2330', start_date='2024-05-01'))
+sid='2330'
+d_per = (datetime.now() - timedelta(days=30)).strftime('%Y-%m-%d')
+df = f.fm_loader.taiwan_stock_per_pbr(stock_id=sid, start_date=d_per)
+print('Length:', len(df) if df is not None else 'None')
+print(df.tail() if df is not None and not df.empty else 'Empty')
---
+++ test2.py
@@ -0,0 +1,5 @@
+from src.backend.web_app import app
+from src.backend.data_fetcher import DataFetcher
+import asyncio
+import requests
+print('Imports OK')
